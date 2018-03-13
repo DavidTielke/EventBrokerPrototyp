2fc4664d2014cf85135c5073ec8f273a0fc49194
David Tielke <mail@david-tielke.de>
2018-03-13 22:21:15 +0100

    Ende Episode 3

--- EventBrokerage.Tests/EventBrokerTests/Subscribe.cs
+++ EventBrokerage.Tests/EventBrokerTests/Subscribe.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DavidTielke.MBH.CrossCutting.EventBrokerage.Tests.EventBrokerTests
@@ -10,9 +11,43 @@ namespace DavidTielke.MBH.CrossCutting.EventBrokerage.Tests.EventBrokerTests
     public partial class EventBrokerTests
     {
         [TestMethod]
-        public void Subscribe_NoSubcriptions_OneSubscription()
+        public void Subscribe_NullAsHandler_ArgumentNullException()
         {
-
+            _broker
+                .Invoking(b => b.Subscribe<TestMessage>(null))
+                .Should()
+                .Throw<ArgumentNullException>();
         }
+
+        [TestMethod]
+        public void Subscribe_NoSubscribtionsButOneAdd_OneSubscription()
+        {
+            Action<TestMessage> handler = msg => msg.Message = "Test";
+
+            _broker.Subscribe(handler);
+
+            _broker.AmountSubscriptions
+                .Should()
+                .Be(1, "one subscription was added.");
+        }
+
+        [TestMethod]
+        public void Subscribe_NoSubscribtionsButTwoAdds_TwoSubscription()
+        {
+            Action<TestMessage> handler1 = msg => msg.Message = "Test";
+            Action<TestMessage> handler2 = msg => msg.Message = "Test";
+
+            _broker.Subscribe(handler1);
+            _broker.Subscribe(handler2);
+
+            _broker.AmountSubscriptions
+                .Should()
+                .Be(2, "two subscription was added.");
+        }
+    }
+
+    public class TestMessage
+    {
+        public string Message { get; set; }
     }
 }
--- EventBrokerage/EventBroker.cs
+++ EventBrokerage/EventBroker.cs
@@ -1,9 +1,37 @@
-﻿using DavidTielke.MBH.CrossCutting.EventBrokerage.Contract;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DavidTielke.MBH.CrossCutting.EventBrokerage.Contract;
 
 namespace DavidTielke.MBH.CrossCutting.EventBrokerage
 {
     public class EventBroker : IEventBroker
     {
+        private readonly Dictionary<Type, List<Delegate>> _subscriptions;
 
+        public EventBroker()
+        {
+            _subscriptions = new Dictionary<Type, List<Delegate>>();
+        }
+
+        public void Subscribe<TMessage>(Action<TMessage> handler)
+        {
+            if (handler == null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            var messageType = typeof(TMessage);
+
+            var messageAlreadyHasSubscribers = _subscriptions.ContainsKey(messageType);
+            if (!messageAlreadyHasSubscribers)
+            {
+                _subscriptions[messageType] = new List<Delegate>();
+            }
+
+            _subscriptions[messageType].Add(handler);
+        }
+
+        public int AmountSubscriptions => _subscriptions.SelectMany(s => s.Value).Count();
     }
 }
